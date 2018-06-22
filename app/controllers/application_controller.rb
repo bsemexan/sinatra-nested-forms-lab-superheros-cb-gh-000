bc907a05669636a7d361ad8f5929693993bb9726
Berry  Semexan <bsemexan@gmail.com>
2018-06-22 23:30:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+
+      params[:team][:members].each do |tm|
+        Member.new(name: tm[:name], power: tm[:power], bio: tm[:bio])
+      end
+      @member = Member.all
+      erb :team
+    end
+
 
 end
