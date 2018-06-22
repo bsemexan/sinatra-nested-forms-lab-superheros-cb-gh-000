0545eb520bebda0919fbc05c2dda5379c80cc977
Berry  Semexan <bsemexan@gmail.com>
2018-06-22 23:17:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,20 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do 
+      erb :super_hero
+    end
+    
+    post '/teams' do
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      
+      params[:team][:members].each do |tm|
+        Member.new(name: tm[:name], power: tm[:power], bio: tm[:bio])
+      end
+      @member = Member.all
+      erb :team
+    end
 
 
 end
